973fd46340b43b220ba65f761b64300e6b4ac616
ZubsZero <zubairmatthee262@gmail.com>
2023-05-19 14:34:10 +0200

    Calculator

---
+++ assets/javascript/index.js
@@ -0,0 +1,21 @@
+let result=document.getElementById("input")
+
+let calculate=(number)=>{
+    result.value+=number;}
+
+let Result=()=>{
+ try{
+    result.value=eval(result.value)
+ }
+ catch(err){
+    alert("Type in number")
+ }
+}
+
+function clr(){
+    result.value="";
+}
+
+function del(){
+    result.value=result.value.slice(0,-1)
+}
